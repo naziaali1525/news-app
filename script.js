76f20f9c8b5f5984a1f24ce8917c87ffaf0723f6
naziaali1525 <38943101+naziaali1525@users.noreply.github.com>
2018-09-06 18:03:02 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -4,20 +4,40 @@ $.ajax({
     success:function(data){
         console.log(data)
 
-        
+  
 
     
 
-        document.querySelector(".slide1 ").innerHTML = `<img src="${data.articles[0].urlToImage}" class="img-div">`;
-        document.querySelector(".slide2 ").innerHTML = `<img src="${data.articles[1].urlToImage}" class="img-div">`;
-        document.querySelector(".slide3 ").innerHTML = `<img src="${data.articles[2].urlToImage}" class="img-div">`;
-        document.querySelector(".slide4 ").innerHTML = `<img src="${data.articles[3].urlToImage}" class="img-div">`;
-        document.querySelector(".slide5").innerHTML = `<img src="${data.articles[4].urlToImage}" class="img-div">`;
-        document.querySelector(".slide6 ").innerHTML = `<img src="${data.articles[5].urlToImage}" class="img-div">`;
-        document.querySelector(".slide7 ").innerHTML = `<img src="${data.articles[6].urlToImage}" class="img-div">`;
-        document.querySelector(".slide8 ").innerHTML = `<img src="${data.articles[7].urlToImage}" class="img-div">`;
-        document.querySelector(".slide9").innerHTML = `<img src="${data.articles[8].urlToImage}" class="img-div">`;
-        document.querySelector(".slide10 ").innerHTML = `<img src="${data.articles[9].urlToImage}" class="img-div">`;
+        document.querySelector(".slide1").style ["background-image"] = `url(${data.articles[0].urlToImage})`;
+        document.querySelector(".get-head1").innerHTML = `<p>${data.articles[0].title}</p>`;
+        
+        document.querySelector(".slide2").style ["background-image"] = `url(${data.articles[1].urlToImage})`;
+        console.log(data.articles[1].title)
+        document.querySelector(".get-head2").innerHTML = `<p>${data.articles[1].title}</p>`;
+        
+        document.querySelector(".slide3").style ["background-image"] = `url(${data.articles[2].urlToImage})`;
+        document.querySelector(".get-head3").innerHTML = `<p>${data.articles[2].title}</p>`;
+        
+        document.querySelector(".slide4 ").style ["background-image"] = `url(${data.articles[3].urlToImage})`;
+        document.querySelector(".get-head4").innerHTML = `<p>${data.articles[3].title}</p>`;
+        
+        document.querySelector(".slide5 ").style ["background-image"] = `url(${data.articles[4].urlToImage})`;
+        document.querySelector(".get-head5").innerHTML = `<p>${data.articles[4].title}</p>`;
+        
+        document.querySelector(".slide6 ").style ["background-image"] = `url(${data.articles[5].urlToImage})`;
+        document.querySelector(".get-head6").innerHTML = `<p>${data.articles[5].title}</p>`;
+        
+        document.querySelector(".slide7 ").style ["background-image"] = `url(${data.articles[6].urlToImage})`;
+        document.querySelector(".get-head7").innerHTML = `<p>${data.articles[6].title}</p>`;
+
+        document.querySelector(".slide8 ").style ["background-image"] = `url(${data.articles[7].urlToImage})`;
+        document.querySelector(".get-head8").innerHTML = `<p>${data.articles[7].title}</p>`;
+        
+        document.querySelector(".slide9 ").style ["background-image"] = `url(${data.articles[8].urlToImage})`;
+        document.querySelector(".get-head9").innerHTML = `<p>${data.articles[8].title}</p>`;
+        
+        document.querySelector(".slide10 ").style ["background-image"]= `url(${data.articles[9].urlToImage})`;
+        document.querySelector(".get-head10").innerHTML = `<p>${data.articles[9].title}</p>`;
         
         for(let i = 0; i < 10; i++){
               document.querySelector(".container-main").innerHTML +=
